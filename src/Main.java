d198f36c93152c42d68f2cdd6b616ceeff8643d5
user <ilya.bubnoff@yandex.ru>
2023-12-14 21:59:38 +0300

    twotasks

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5000;
+        System.out.println("Значение переменной а с типом int равно " +a);
+  byte b = 125;
+        System.out.println("Значение переменной b с типом byte равно " +b);
+ short c = -32300;
+        System.out.println("Значение переменной c с типом short равно " +c);
+ long d = 999999998;
+        System.out.println("Значение переменной c с типом long равно " +d);
+float e = 3.14f;
+        System.out.println("Значение переменной e с типом float равно " +e);
+double f = 31.5897;
+        System.out.println("Значение переменной f с типом double равно " +f);
+float cat = 27.12f;
+long horse = 987678965549L;
+double dog = 2.786;
+short dolphin = 569;
+char came = 569;
+int pressure = -159;
+short money = 27897;
+byte cow = 67;
+
+    }
+}
