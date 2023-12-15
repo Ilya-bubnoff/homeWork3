7e8d19a971de2f650d3e1c43a6e8eabc3af9d7c4
user <ilya.bubnoff@yandex.ru>
2023-12-15 21:57:19 +0300

    fourtasks

--- src/Main.java
+++ src/Main.java
@@ -20,6 +20,23 @@ char came = 569;
 int pressure = -159;
 short money = 27897;
 byte cow = 67;
+byte classA = 23;
+byte classB = 27;
+byte classD = 30;
+int sheetsofPaper = 480;
+int sheet = sheetsofPaper/(classA+classB+classD);
+System.out.println("На каждого ученика расчитано " +sheet+ " листов бумаги");
+byte bottles = 16;
+byte time = 2;
+int productivity1 = bottles/time;
+int productivity20 = productivity1*20;
+System.out.println("За 20 минут машина произвела "+productivity20+ " штук бутылок");
+int productivityDay = productivity1*24*60;
+        System.out.println("За сутки машина произвела "+productivityDay+ " штук бутылок");
+int productivityDay_3 = productivity1*24*60*3;
+        System.out.println("За трое сутк машина произвела "+productivityDay_3+ " штук бутылок");
+long productivityMonth = productivity1*24*60*31;
+        System.out.println("За месяц машина произвела "+productivityMonth+ " штук бутылок");
 
     }
 }
